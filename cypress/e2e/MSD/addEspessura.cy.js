f601cd36926bfa64629a5e26cc043f011a501754
jadiaelns1 <jadiael@edge.ufal.br>
2023-07-09 21:35:59 -0300

    novo adição teste

---
+++ cypress/e2e/MSD/addEspessura.cy.js
@@ -0,0 +1,7 @@
+/* ==== Test Created with Cypress Studio ==== */
+it('Deve cadastrar uma nova espessura', function() {
+  /* ==== Generated with Cypress Studio ==== */
+  cy.visit('http://3.144.196.238/maintenance/msd_materials/msd/thickness_config');
+  cy.ntlm(["3.144.196.238"], "usuario_msd", "Edge@123!", "");
+  /* ==== End Cypress Studio ==== */
+});
